7de1f1a69874307c323ac82bdc145d3eee174a0e
nanyatti <34296375+nanyatti@users.noreply.github.com>
2018-02-20 20:17:23 +0900

    READMEを追加。

---
+++ README.md
@@ -0,0 +1,6 @@
+# opencvsharp-wpf
+画像処理ライブラリOpenCVをC#で扱える
+
+OpenCVSharp3を使ってみた。
+
+簡単に画像を表示する。
--- opencvsharp-wpf/MainWindow.xaml.cs
+++ opencvsharp-wpf/MainWindow.xaml.cs
@@ -13,16 +13,32 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+
 namespace opencvsharp_wpf
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            src = new Mat(@"C:\Users\tsutsumi\Downloads\pepper素材\pepper.jpg");
+            imgResult.Source = src.ToWriteableBitmap();
+        }
+
+        Mat src;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //var src = new Mat(@"C:\Users\tsutsumi\Downloads\pepper素材\pepper.jpg");
+            Mat dst = new Mat();
+            Cv2.CvtColor(src, dst, ColorConversionCodes.BGRA2GRAY);
+            imgResult.Source = dst.ToWriteableBitmap();
         }
     }
 }
